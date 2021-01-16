8d02076f28e4a9973b71183e7b58e8c0baa51a0e
Michele Mastrogiovanni <michele.mastrogiovanni@gmail.com>
2021-01-16 12:17:08 +0100

    Added health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,16 @@
 import { Body, Controller, Get, Post, Query, Req } from '@nestjs/common';
+import { AlgorandService } from './algorand.service';
 import { AppService } from './app.service';
 import { RequestDto } from './request.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get("/health")
+  health() {
+    return "OK"
+  }  
 
   @Post()
   tokenize(@Body() request: RequestDto) {
